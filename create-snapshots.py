b697fb8e8d0963759beefe3348dff3e16980c299
donfortune <donfortunet.df@gmail.com>
2023-02-18 08:39:14 +0100

    added volumes backup

---
+++ create-snapshots.py
@@ -0,0 +1,31 @@
+import boto3
+import schedule
+
+client = boto3.client('ec2', region_name='us-east-2')
+def automate_snapshot_creation():
+    volumes = client.describe_volumes(
+        Filters=[
+            {
+                'Name': 'tag:name',
+                'Values': [
+                    'prod',
+                ]
+            },
+        ],
+    )
+    print(volumes['Volumes'])
+
+    for volume in volumes['Volumes']:
+        snapshots = client.create_snapshot(
+            VolumeId=volume['VolumeId']
+        )
+
+        print(snapshots)
+
+schedule.every(20).seconds.do(automate_snapshot_creation)
+
+while True:
+    schedule.run_pending()
+
+#schedule to clean up snapshots, only need most recent snapshot
+
